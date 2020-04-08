3f3d4a91729a201a4e44a20c3d2dd62c66a7a431
SheikAhamed85 <58125551+SheikAhamed85@users.noreply.github.com>
2020-04-08 19:52:07 +0100

    Donations pagination changes

--- src/views/help/Donations.vue
+++ src/views/help/Donations.vue
@@ -1,10 +1,15 @@
 <template>
   <div>
-    <div class="container mt-4">
-      <div
+    <div class="container mt-2">
+        <div class="row">
+            <div class="col-sm-12">
+            <b-breadcrumb :items="breadcrumbs" />
+        </div> 
+     </div>
+     <div
         v-if="user && user.loggedIn && user.data && (user.data.moderator || user.data.verifiedvolunteer)"
       >
-        <div class="row mt-4">
+        <div class="row mt-2">
           <div class="col-sm-12">
             <div class="card">
               <div class="card-body">
@@ -24,7 +29,18 @@
         <div class="row">
           <div class="col-sm-12" v-if="donations">
             <div class="card">
-              <div class="card-header">Donations</div>
+              <div class="card-header"><span class="btn btn-primary"><b>Donations</b></span>
+                          <span v-if="pageNavButton">
+                                <button type="button" class="btn btn-primary float-right" @click="lastPage()" v-if="lastButtonStatus">&gt;&gt;</button>
+                                <button type="button" class="btn btn-primary float-right" v-if="!lastButtonStatus">&gt;&gt;</button>
+                                <button type="button" class="btn btn-primary float-right ml-1" @click="nextPage()" v-if="nextNavStatus">&gt;</button>
+                                <button type="button" class="btn btn-primary float-right ml-1" v-if="!nextNavStatus">&gt;</button>
+                                <button type="button" class="btn btn-primary float-right" @click="prevPage()" v-if="prevNavStatus">&lt;</button>
+                                <button type="button" class="btn btn-primary float-right" v-if="!prevNavStatus">&lt;</button>
+                                <button type="button" class="btn btn-primary float-right" @click="fetchDonations()" v-if="firstButtonStatus">&lt;&lt;</button>
+                                <button type="button" class="btn btn-primary float-right" v-if="!firstButtonStatus">&lt;&lt;</button>
+                          </span>
+              </div>
               <div class="card-body">
                 <div v-for="donation in donations" :key="donation.id">
                   <span class="text-primary" style="font-size:16px;font-weight:bold;">
@@ -36,7 +52,7 @@
                     style="font-size:16px;font-weight:light;"
                   >{{donation.data.donation_status || 'new'}}</span>                  
                   <br />
-                  Donor - {{ donation.data.contact.name || donation.data.contact.email }} entered by
+                  Donor - {{ donation.data.contact.firstname + " " + donation.data.contact.lastname || donation.data.contact.email }} entered by
                   <i>{{donation.data.user_displayName || donation.data.user_email || 'Unknown User' }}</i>
                   <br />
                   <p>{{donation.data.donation.message}}</p>
@@ -63,7 +79,21 @@ var db = firebase.firestore();
 export default {
   data() {
     return {
-      donations: null
+      donations: null,
+      filterStatus: "all",
+      pageSize:5,
+      loadedFrom: "base",
+      firstDonation: null,
+      lastDonation: null,
+      pageNavButton: false,
+      firstButtonStatus: false,
+      prevNavStatus: false,
+      nextNavStatus: true,
+      lastButtonStatus: true,
+      breadcrumbs : [
+                { text : "Home", to : "/" },
+                { text : "Donations", active : true }
+            ],
     };
   },
   watch: {
@@ -72,65 +102,133 @@ export default {
     }
   },
   methods: {
-
-    getAllDonations() {
-      if(this.user.data.moderator){
-         return db
-        .collection("donations")            
-        .orderBy("timestamp", "desc")
-        .limit(50)
-        .get();         
-      }
-      return db
-        .collection("donations")
-        .where("picked_up_by", "==", this.user.data.email)        
-        .orderBy("timestamp", "desc")
-        .limit(50)
-        .get();
-          
+    getDonations() {
+            let donationList = null
+            if(this.user.data.moderator){
+              donationList = db
+                .collection("donations")
+            } else {
+              donationList = db
+                .collection("donations")
+                .where("picked_up_by", "==", this.user.data.email)
+            }
+            if (this.filterStatus === "new") {
+                donationList = donationList.where("donation_status", "==", this.filterStatus)
+            } else if (this.filterStatus === "assigned") {
+                donationList = donationList.where("donation_status", "==", this.filterStatus)
+            } else if (this.filterStatus === "fulfilled") {
+                donationList = donationList.where("donation_status", "==", this.filterStatus)
+            }
+            return donationList;
     },
-    getDonationsByStatus(s) {
-      if(this.user.data.moderator){
-        return db
-        .collection("donations")
-        .where("donation_status", "==", s)
+    getDonationsByParam() {
+       return this.getDonations()   
         .orderBy("timestamp", "desc")
-        .limit(50)
-        .get();        
-      }
-      return db
-        .collection("donations")
-        .where("donation_status", "==", s)
-        .where("picked_up_by", "==", this.user.data.email)
-        .orderBy("timestamp", "desc")
-        .limit(50)
-        .get();      
+        .limit(this.pageSize)
+        .get();  
     },
     fetchDonations() {
-      const filter_status = this.$route.params.status;
+      if(this.$route.params.status)
+      this.filterStatus = this.$route.params.status;
       let _donations = [];
-      if (!filter_status || filter_status === "all") {
-        this.getAllDonations().then(querySnapshot => {
-          querySnapshot.forEach(doc => {
-            _donations.push({
-              id: doc.id,
-              data: doc.data()
-            });
-          });
-          this.donations = _donations;
+        this.getDonationsByParam().then(querySnapshot => {
+          this.donations = this.getDonationResults(querySnapshot);
+          if(this.donations.length <this.pageSize) {
+           this.pageNavButton=false;
+          } else {
+          this.pageNavButton=true;
+          this.firstButtonStatus=false;
+          this.prevNavStatus=false;
+          this.nextNavStatus=true;
+          this.lastButtonStatus=true;
+          }
         });
-      } else {
-        this.getDonationsByStatus(filter_status).then(querySnapshot => { 
-          querySnapshot.forEach(doc => {
-            _donations.push({
-              id: doc.id,
-              data: doc.data()
+    },nextPage() {
+		    let _donations = [];
+            this.loadedFrom = "next";
+            return this.getDonations()
+                .orderBy("timestamp","desc")
+                .startAfter(this.lastDonation)
+                .limit(this.pageSize)
+                .get()
+                .then(querySnapshot => {
+                    this.donations = this.getDonationResults(querySnapshot);
+                    if (this.donations.length < this.pageSize) {
+                        this.lastPage(this.filterStatus);
+                        this.firstButtonStatus=true;
+                        this.prevNavStatus=true;
+                        this.nextNavStatus=false;
+                        this.lastButtonStatus=false;
+                    } else {
+                        this.firstButtonStatus=true;
+                        this.prevNavStatus=true;
+                        this.nextNavStatus=true;
+                        this.lastButtonStatus=false;
+                    }
+                });
+        },
+        prevPage() {
+            let _donations = [];
+            this.loadedFrom = "prev";
+            return this.getDonations()
+                .orderBy("timestamp","desc")
+                .endBefore(this.firstDonation)
+                .limitToLast(this.pageSize)
+                .get()
+                .then(querySnapshot => {
+				            this.donations = this.getDonationResults(querySnapshot);
+                    if (this.donations.length < this.pageSize) {
+                        this.fetchDonations();
+                        this.firstButtonStatus=false;
+                        this.prevNavStatus=true;
+                        this.nextNavStatus=true;
+                        this.lastButtonStatus=true;
+                    } else {
+                        this.firstButtonStatus=true;
+                        this.prevNavStatus=true;
+                        this.nextNavStatus=true;
+                        this.lastButtonStatus=true;
+                    }
+                });
+        },
+        lastPage() {
+		    let _donations = [];
+            this.loadedFrom = "base";
+            return this.getDonations()
+                .orderBy("timestamp", "asc")
+                .limit(this.pageSize)
+                .get()
+                .then(querySnapshot => {
+                this.donations = this.getDonationResults(querySnapshot);
+                this.firstButtonStatus=true;
+                this.prevNavStatus=true;
+                this.nextNavStatus=false;
+                this.lastButtonStatus=false;
+                });                 
+        }, 
+        getDonationResults(querySnapshot) {
+            let _donations = [];
+            let i = 0;
+            querySnapshot.forEach(doc => {
+                if (i == 0) {
+                    this.firstDonation = doc;
+                }
+
+                if (i == (this.pageSize - 1)) {
+                    this.lastDonation = doc;
+                }
+                i++;
+                _donations.push({
+                    id: doc.id,
+                    data: doc.data()
+                });
             });
-          });
-          this.donations = _donations;
-        });
-      }
-    }
+
+            this.donations = _donations;
+
+            return _donations;
+
+        }
   },
   created() {
       this.fetchDonations();
